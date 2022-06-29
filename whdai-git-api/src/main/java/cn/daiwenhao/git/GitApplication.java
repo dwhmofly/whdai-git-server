937d348b0f659fd3c9c18892f5a71175579a0d58
whdai <dwh@daiwenhao.cn>
2022-06-29 16:43:16 +0800

    gradle升级，springboot升级

--- whdai-git-api/src/main/java/cn/daiwenhao/git/GitApplication.java
+++ whdai-git-api/src/main/java/cn/daiwenhao/git/GitApplication.java
@@ -2,8 +2,10 @@ package cn.daiwenhao.git;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity
 public class GitApplication {
 
     public static void main(String[] args) {
--- whdai-git-api/src/main/java/cn/daiwenhao/git/SecurityConfiguration.java
+++ whdai-git-api/src/main/java/cn/daiwenhao/git/SecurityConfiguration.java
@@ -1,13 +1,15 @@
 package cn.daiwenhao.git;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests().anyRequest().authenticated().and().httpBasic();
+public class SecurityConfiguration {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.csrf().disable().authorizeRequests().anyRequest().authenticated().and().httpBasic().and().build();
     }
 }
